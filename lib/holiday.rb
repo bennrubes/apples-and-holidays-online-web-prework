15199834dbd6560c762d7ecbd31ed4207658fe6d
Sam Bennrubi <sbennrubi@gmail.com>
2018-11-12 00:31:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each {|season, holidays| 
     puts "#{season}:".to_s.capitalize 
     holidays.each {|holiday, supplies| 
-      puts "#{supplies.join(",")}"}}
+      puts "#{holiday}:"}}
   
 end
 
